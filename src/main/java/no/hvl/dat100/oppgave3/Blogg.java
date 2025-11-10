c769199b479f16c6369b5db0ca63e598f4042066
DrTellus <102365316+DrTellus@users.noreply.github.com>
2025-11-10 09:41:19 +0100

    Oppgave 3 og 4

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -1,72 +1,76 @@
 package no.hvl.dat100.oppgave3;
 
-import no.hvl.dat100.common.TODO;
+import no.hvl.dat100.oppgave1.Innlegg;
 import no.hvl.dat100.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+    //Oppgave a
+    private Innlegg[] innleggsTabell;
+    private int nesteLedig;
 
+
+
+    //oppgave b
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+        innleggsTabell = new Innlegg[20];
+        nesteLedig = 0;
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggsTabell = new Innlegg[lengde];
+        nesteLedig = 0;
 	}
 
+    //oppgave c
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		return nesteLedig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		return innleggsTabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+        for (int i = 0; i<nesteLedig; i++){
+            if(innleggsTabell[i].erLik(innlegg)){
+                return 1;
+            }
+        }
+        return -1;
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		return finnInnlegg(innlegg) !=-1;
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		return nesteLedig < innleggsTabell.length;
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-
-	// valgfrie oppgaver nedenfor
-	
-	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public boolean leggTilUtvid(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-	
-	public boolean slett(Innlegg innlegg) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public int[] search(String keyword) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+        if (finnes(innlegg)){
+            return false;
+        }
+        if (!ledigPlass()){
+            return false;
+        }
+        innleggsTabell[nesteLedig] = innlegg;
+        nesteLedig++;
+        return true;
 
 	}
-}
+        @Override
+        public String toString() {
+            //Oppgave i
+            String resultat = nesteLedig + "\n";
+            for(int i = 0; i<nesteLedig; i++){
+                resultat = resultat + innleggsTabell[i].toString();
+            }
+            return resultat;
+        }
+
+
+
+	}
--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -10,6 +10,16 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+        try{
+            System.out.println("Skriver til fil : " + mappe + filnavn);
+            System.out.println("Innhold");
+            System.out.println(samling.toString());
+            return true;
+        }
+        catch (Exception e) {
+            System.out.println("Noe gikk galt : " + e.getMessage());
+            return false;
+        }
+
 	}
 }
